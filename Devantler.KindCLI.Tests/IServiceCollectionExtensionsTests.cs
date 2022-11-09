a5c81a0393c33bd2a734206f1e346824f077e580
Nikolai Emil Damm <nikolaemildamm@icloud.com>
2022-11-09 02:16:07 +0100

    Added test for service collection extension

---
+++ Devantler.KindCLI.Tests/IServiceCollectionExtensionsTests.cs
@@ -0,0 +1,21 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace Devantler.KindCLI.Tests;
+
+public class IServiceCollectionExtensionsTests
+{
+    [Fact]
+    public void AddKindCliService_RegistersScopedService()
+    {
+        var services = new ServiceCollection();
+        var configuration = new ConfigurationBuilder().Build();
+
+        services.AddKindCliService();
+
+        var _ = Assert.Single(services, x =>
+            x.ServiceType == typeof(IKindCliService) &&
+            x.Lifetime == ServiceLifetime.Scoped);
+    }
+}
